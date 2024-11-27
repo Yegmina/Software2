88d8b0361d6949c11da86607717752cb0f8932d8
Yehor <te35603560@gmail.com>
2024-11-27 09:41:47 +0200

    class_work_example

---
+++ python/class_work_4/example/example.js
@@ -0,0 +1,26 @@
+// example.js
+'use strict';
+
+const lat = 60;
+const lon = 21;
+const APIkey = '7bfbeb2ebaec8cdb59103f744a3e8c1f';
+
+const request = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=metric`;
+
+async function fetchWeather() {
+  try {
+    const response = await fetch(request);
+    if (!response.ok) {
+      throw new Error('Resource not found');
+    }
+    const weather = await response.json();
+    const name = weather.name;
+    const temp = weather.main.temp;
+    console.log(`Name: ${name}`);
+    console.log(`Temperature: ${temp} Celsius`);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+fetchWeather();
---
+++ python_labs/test/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+x=np.arange(-5,0)
+y=2.0**x
+
+print(y)
